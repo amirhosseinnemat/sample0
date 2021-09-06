a91a7d3c12ef8b7e9d404b41ea2a2760aba268cf
amir <ahosseinnemat@gmail.com>
2021-09-07 00:02:45 +0430

    add java doc

---
+++ 003-java/0020-java-doc/src/com/amir/javaSample/Main.java
@@ -0,0 +1,17 @@
+package com.amir.javaSample;
+
+import java.util.Arrays;
+
+/**
+ * @author amir
+ * This is a simple java doc
+ * this program just print a hello world
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        int[][] arr = {{2,4,6},{8,10,12}};
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
